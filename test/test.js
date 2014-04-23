c7645b4d4398cb92d3fb2a37c4e1a420f3731268
Kevin Martensson <kevinmartensson@gmail.com>
2014-04-23 14:02:19 +0200

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,23 @@
+/*global describe, it */
+'use strict';
+
+var assert = require('assert');
+var fs = require('fs');
+var gifsicle = require('../');
+var Imagemin = require('image-min');
+var path = require('path');
+
+describe('gifsicle()', function () {
+    it('should optimize a JPG', function (cb) {
+        var imagemin = new Imagemin();
+
+        imagemin
+            .src(path.join(__dirname, 'fixtures/test.gif'))
+            .use(gifsicle())
+            .optimize(function (err, file) {
+                assert(file.contents.length < fs.statSync(imagemin.src()).size);
+                assert(file.contents.length > 0);
+                cb();
+            });
+    });
+});
