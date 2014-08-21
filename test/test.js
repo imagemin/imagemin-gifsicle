69cfd96039fb913d24ddc2a458225b30cf8a2e53
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-22 01:59:28 +0200

    Use ava for testing

--- test/test.js
+++ test/test.js
@@ -1,23 +1,25 @@
-/*global describe, it */
 'use strict';
 
-var assert = require('assert');
 var fs = require('fs');
 var gifsicle = require('../');
 var Imagemin = require('imagemin');
 var path = require('path');
+var test = require('ava');
 
-describe('gifsicle()', function () {
-    it('should optimize a GIF', function (cb) {
-        var imagemin = new Imagemin();
+test('should optimize a GIF', function (t) {
+	t.plan(4);
 
-        imagemin
-            .src(path.join(__dirname, 'fixtures/test.gif'))
-            .use(gifsicle())
-            .optimize(function (err, file) {
-                assert(file.contents.length < fs.statSync(imagemin.src()).size);
-                assert(file.contents.length > 0);
-                cb();
-            });
-    });
+	var imagemin = new Imagemin()
+		.src(path.join(__dirname, 'fixtures/test.gif'))
+		.use(gifsicle());
+
+	imagemin.optimize(function (err, file) {
+		t.assert(!err);
+
+		fs.stat(imagemin.src(), function (err, stats) {
+			t.assert(!err);
+			t.assert(file.contents.length < stats.size);
+			t.assert(file.contents.length > 0);
+		});
+	});
 });
