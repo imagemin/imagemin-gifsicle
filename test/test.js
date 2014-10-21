662ced1acbb57f3a92fedb57f5bb36782f525520
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-21 16:17:45 +0200

    Provide a reusable constructor

--- test/test.js
+++ test/test.js
@@ -26,3 +26,25 @@ test('optimize a GIF', function (t) {
 		stream.end(file);
 	});
 });
+
+test('optimize a GIF using ctor', function (t) {
+	t.plan(3);
+
+	var Gifsicle = gifsicle.ctor();
+
+	fs.readFile(path.join(__dirname, 'fixtures/test.gif'), function (err, buf) {
+		t.assert(!err);
+
+		var stream = new Gifsicle();
+		var file = new File({
+			contents: buf
+		});
+
+		stream.on('data', function (data) {
+			t.assert(data.contents.length < buf.length);
+			t.assert(isGif(data.contents));
+		});
+
+		stream.end(file);
+	});
+});
