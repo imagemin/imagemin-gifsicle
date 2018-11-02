17ad43d3a5a992f4dbdb60fa195f0e42f534daa4
evilebottnawi <sheo13666q@gmail.com>
2018-11-02 13:00:48 +0300

    fix: error when `string` passed

--- test.js
+++ test.js
@@ -5,9 +5,15 @@ import pify from 'pify';
 import test from 'ava';
 import m from '.';
 
-test(async t => {
+test('Buffer', async t => {
 	const buf = await pify(fs.readFile)(path.join(__dirname, 'fixture.gif'));
 	const data = await m()(buf);
 	t.true(data.length < buf.length);
 	t.true(isGif(data));
+});
+
+test('String', async t => {
+	const buf = Buffer.from('string');
+	const data = await m()(buf);
+	t.true(data.toString() === 'string');
 });
